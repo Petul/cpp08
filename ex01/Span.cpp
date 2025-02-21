eb953cc04b958fe34e8b85eb211a4acc4bdcbf93
Peter Leander <pleander@student.hive.fi>
2025-02-21 14:33:51 +0200

    Fix error when finding min span

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -61,7 +61,7 @@ unsigned int Span::shortestSpan()
 	{
 		throw std::runtime_error("Error: Span is empty");
 	}
-	std::vector<unsigned int> diff(this->size_ - 1);
+	std::vector<unsigned int> diff(this->vec_.size() - 1);
 	std::adjacent_difference(this->vec_.begin(), this->vec_.end(),
 	                         diff.begin());
 	return (*std::min_element(diff.begin(), diff.end()));
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -18,62 +18,80 @@
 
 int main(void)
 {
-	srand(time(0));
+	try
 	{
-		std::cout << "Subject example: " << std::endl;
-		Span sp = Span(5);
-		sp.addNumber(6);
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
-		std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
-		std::cout << "Longest span: " << sp.longestSpan() << std::endl;
-	}
-	{
-		std::cout << "\nLarge numbers: " << std::endl;
-		Span sp = Span(5);
-		sp.addNumber(6);
-		sp.addNumber(std::numeric_limits<int>::max());
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(std::numeric_limits<int>::min());
-		std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
-		std::cout << "Longest span: " << sp.longestSpan() << std::endl;
-	}
-	{
-		std::cout << "\nAdding 10 000 numbers" << std::endl;
-		Span s(10000);
-		for (int i = 0; i < 10000; i++)
+		srand(time(0));
 		{
-			s.addNumber(std::rand());
+			std::cout << "Subject example: " << std::endl;
+			Span sp = Span(5);
+			sp.addNumber(6);
+			sp.addNumber(3);
+			sp.addNumber(17);
+			sp.addNumber(9);
+			sp.addNumber(11);
+			std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+			std::cout << "Longest span: " << sp.longestSpan() << std::endl;
 		}
-		std::cout << "Longest span: " << s.longestSpan() << std::endl;
-		std::cout << "Shortest span: " << s.shortestSpan() << std::endl;
-	}
-	{
-		std::cout << "\nAdding a range of numbers" << std::endl;
-		Span s(10000);
-		std::vector<int> vec;
-		for (int i = 0; i < 10000; i++)
 		{
-			vec.push_back(std::rand());
+			std::cout << "\nLonger span than numbers: " << std::endl;
+			Span sp = Span(10);
+			sp.addNumber(6);
+			sp.addNumber(3);
+			sp.addNumber(17);
+			sp.addNumber(9);
+			sp.addNumber(11);
+			std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+			std::cout << "Longest span: " << sp.longestSpan() << std::endl;
 		}
-		s.addNumbers(vec.begin(), vec.end());
-		std::cout << "Longest span: " << s.longestSpan() << std::endl;
-		std::cout << "Shortest span: " << s.shortestSpan() << std::endl;
-	}
+		{
+			std::cout << "\nLarge numbers: " << std::endl;
+			Span sp = Span(5);
+			sp.addNumber(6);
+			sp.addNumber(std::numeric_limits<int>::max());
+			sp.addNumber(17);
+			sp.addNumber(9);
+			sp.addNumber(std::numeric_limits<int>::min());
+			std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+			std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+		}
+		{
+			std::cout << "\nAdding 10 000 numbers" << std::endl;
+			Span s(10000);
+			for (int i = 0; i < 10000; i++)
+			{
+				s.addNumber(std::rand());
+			}
+			std::cout << "Longest span: " << s.longestSpan() << std::endl;
+			std::cout << "Shortest span: " << s.shortestSpan() << std::endl;
+		}
+		{
+			std::cout << "\nAdding a range of numbers" << std::endl;
+			Span s(10000);
+			std::vector<int> vec;
+			for (int i = 0; i < 10000; i++)
+			{
+				vec.push_back(std::rand());
+			}
+			s.addNumbers(vec.begin(), vec.end());
+			std::cout << "Longest span: " << s.longestSpan() << std::endl;
+			std::cout << "Shortest span: " << s.shortestSpan() << std::endl;
+		}
 
-	{
-		std::cout << "\nAdding another range of numbers" << std::endl;
-		Span s(10000);
-		std::array<int, 10000> arr;
-		for (int i = 0; i < 10000; i++)
 		{
-			arr[i] = std::rand();
+			std::cout << "\nAdding another range of numbers" << std::endl;
+			Span s(10000);
+			std::array<int, 10000> arr;
+			for (int i = 0; i < 10000; i++)
+			{
+				arr[i] = std::rand();
+			}
+			s.addNumbers(arr.begin(), arr.end());
+			std::cout << "Longest span: " << s.longestSpan() << std::endl;
+			std::cout << "Shortest span: " << s.shortestSpan() << std::endl;
 		}
-		s.addNumbers(arr.begin(), arr.end());
-		std::cout << "Longest span: " << s.longestSpan() << std::endl;
-		std::cout << "Shortest span: " << s.shortestSpan() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
 	}
 }
